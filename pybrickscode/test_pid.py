2818e127cb13f2f965108a7b6237b987d14b3833
Shoupu Wan <swan@roblox.com>
2023-09-14 09:41:17 -0700

    check in a test with pid tuning and motor testing

--- pybrickscode/test_pid.py
+++ pybrickscode/test_pid.py
@@ -4,11 +4,11 @@ it has HEADING code!
 *hub_imu = hub.imu.heading()*
 https://beta.pybricks.com/
 """
+from pybricks.parameters import Color
+
 ik = 0.01
 pk = 1
 delta = .3
-
-
 
 def test():
     from fll_robot import Robot
@@ -16,7 +16,7 @@ def test():
     wheels = robot.motor_pair
     heading_control = wheels.heading_control
     h_old_pid = heading_control.pid()
-    # heading_control.pid(1, 1, 1)
+    heading_control.pid(14000, 0, 1000)
 
     h_old_limits = heading_control.limits()
     # heading_control.limits(1, 1, 1)
@@ -27,7 +27,7 @@ def test():
     distance_control = wheels.distance_control
 
     d_old_pid = distance_control.pid()
-    # distance_control.pid(2, 2, 2)
+    distance_control.pid(14000, 2, 2000)
 
     d_old_limits = distance_control.limits()
     # distance_control.limits(1, 1, 1)
@@ -37,7 +37,13 @@ def test():
 
     print(f'{h_old_pid}, {h_old_limits}')
     print(f'{d_old_pid}, {d_old_limits}')
-    wheels.turn(90)
-
+    while True:
+        distance = 500
+        # wheels.turn(180)
+        # while not robot.left_sensor.color(Color.RED):
+        wheels.straight(distance)
+        robot.left_motor.run_time(-1000, 1000, wait=False)
+        robot.right_motor.run_time(-5000, 1000)
+        wheels.turn(90)
 
 test()
