88f892fbd75117f8866752a957aab939e751cfbf
Samuel Wan <wanule123@gmail.com>
2023-09-23 14:21:33 -0700

    i did some cool pentagon highly classified work

--- pybrickscode/mission_04_masterpiece.py
+++ pybrickscode/mission_04_masterpiece.py
@@ -4,18 +4,24 @@ from fll_robot import Robot
 from lib_turn import gyro_turn
 from lib_move import move
 from lib_line_follow import line_follow
+from lib_polygon import polygon
+
 
 def run(bot: Robot):
     wheels = bot.motor_pair
-    wheels.straight(510)
-    wheels.curve(0,-69.49)
-    wheels.straight(850)
-    wheels.straight(-830)
-    wheels.curve(0,69.49)
-    wheels.straight(-510)
+    polygon(0, vertices=[(0, 960), (700, 600), (880, 180)])
+    # wheels.straight(510)
+    # wheels.curve(0,-66)
+    # wheels.straight(850)
+    # wheels.straight(-830)
+    # wheels.curve(0,69.3)
+    # wheels.straight(-510)
+
+
 def main():
     bot = Robot()
     run(bot)
+
 
 if __name__ == "__main__":
     main()
