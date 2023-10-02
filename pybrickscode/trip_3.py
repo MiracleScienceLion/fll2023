12ed2339949189afc84f1346e46f44408810e687
Samuel Wan <wanule123@gmail.com>
2023-10-01 20:58:23 -0700

    i did some coding and it wasted about 2-3 hours of my life

--- pybrickscode/trip_3.py
+++ pybrickscode/trip_3.py
@@ -9,7 +9,16 @@ from lib_polygon import polygon
 
 def run(bot: Robot):
     bot.reset_heading(0)
-    polygon(bot, vertices=[(300,-880)], forward=True, reverse=True)
+    polygon(bot, vertices=[(0,-900),(790,-670),(850,-580)])
+    polygon(bot, vertices=[(850,-580),(730,-730)], forward=False)
+    polygon(bot, vertices=[(730,-730),(740,-740)])
+    # for i in range(1):
+        # polygon(bot, vertices=[(680,-650),(730,-730)], reverse=True)
+    # polygon(bot, vertices=[(640,-760),(700,-630)])
+    # polygon(bot, vertices=[(700,-630),(720,-700)],reverse=True)
+    # polygon(bot, vertices=[(720,-700),(910,60),(840,60)])
+    # polygon(bot, vertices=[(840,60),(910,60)], forward=False)
+    # polygon(bot, vertices=[(910,60),(880,120),(940,180),(890,520),(290,950)])
 def main():
     bot = Robot()
     run(bot)
