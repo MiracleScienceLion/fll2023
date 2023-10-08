206f0a08f9450f3edf43e402e4ab15733c2419f7
Samuel Wan <wanule123@gmail.com>
2023-10-07 18:54:20 -0700

    code to pick up sam

---
+++ pybrickscode/pickup_sam.py
@@ -0,0 +1,30 @@
+from pybricks.parameters import Button, Color, Direction, Icon, Port, Side, Stop
+
+from fll_robot import Robot
+from lib_turn import gyro_turn
+from lib_move import move
+from lib_line_follow import line_follow
+from lib_polygon import polygon
+
+motor_gear = 12
+load_gear = 20
+gear_ratio = load_gear / motor_gear
+
+deploy_angle = 94 * gear_ratio
+
+rotation_speed = 300
+
+
+def deploy(bot: Robot):
+    bot.right_motor.run_angle(rotation_speed, deploy_angle)
+
+
+def undeploy(bot:Robot):
+    bot.right_motor.run_angle(rotation_speed, -deploy_angle)
+
+
+if __name__ == "__main__":
+    from pybricks.tools import wait
+    bot = Robot()
+    deploy(bot)
+    undeploy(bot)
