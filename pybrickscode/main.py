effe1ccd50e3616877237d01e9279f57b17e1d18
David Tian <David.Tian@gmail.com>
2023-10-08 23:58:21 -0700

    Add program selection to hub.

--- pybrickscode/main.py
+++ pybrickscode/main.py
@@ -7,50 +7,102 @@ from pybricks.tools import wait, StopWatch
 from fll_robot import Robot
 from lib_turn import gyro_turn
 from lib_move import move
-from lib_line_follow import line_follow
+from lib_logger import Logger
 
-def left_button_event(bot):
-    print('left')
-    bot.right_motor.run_angle(100, 180, wait=False)
-    bot.left_motor.run_angle(100, -360, wait=False)
-    line_follow(bot, speed=200, time_out_ms=3000)    
+############### CHANGE CONTENT BELOW ###############
+def trip_01(bot):
+    logger.info('trip_01')
 
-def right_button_event(bot):
-    print('right')
-    gyro_turn(bot, angle=45, speed=200)
+def trip_02(bot):
+    logger.info('trip_02')
 
-def bluetooth_button_event(bot):
-    print('bluetooth')
-    bot.hub.display.icon(icon=Icon.HAPPY)
-    bot.right_motor.reset_angle(0)
-    print('angle before',bot.right_motor.angle())
-    bot.right_motor.run_angle(100, 180, wait=True)
-    print('angle after',bot.right_motor.angle())
-    wait(3000)
-    print('angle after wait',bot.right_motor.angle())
-    bot.right_motor.run_target(100, 50)
-    print('angle after run_target',bot.right_motor.angle())
-    bot.left_motor.run_angle(100, -360, wait=False)
-    bot.motor_pair.straight(distance=500)  # Built-in gyro straight!!!
-    bot.hub.speaker.beep()
+def trip_03(bot):
+    logger.info('trip_03')
 
-def center_button_event(bot):
-    print('center and stop')
+def trip_04(bot):
+    logger.info('trip_04')
+
+def trip_05(bot):
+    logger.info('trip_05')
+
+def trip_06(bot):
+    logger.info('trip_06')
+
+def trip_07(bot):
+    logger.info('trip_07')
+
+def trip_08(bot):
+    logger.info('trip_08')
+############### CHANGE CONTENT ABOVE ###############
+
+############### DO NOT CHANGE BELOW ###############
+TRIP_START = 1
+TRIP_END = 8
+
+logger = Logger(Logger.DEBUG)
+
+def left_button_event(bot, trip_num) -> int:
+    logger.info('left_button_event')
+    if trip_num <= TRIP_START:
+        logger.warning('At leftmost already')
+        return trip_num
+    return trip_num - 1
+    
+def right_button_event(bot, trip_num) -> int:
+    logger.info('right_button_event')
+    if trip_num >= TRIP_END:
+        logger.warning('At rightmost already')
+        return trip_num
+    return trip_num + 1
+
+def bluetooth_button_event(bot, trip_num) -> int:
+    logger.info('bluetooth_button_event and stop')
+    return trip_num
+    
+def center_button_event(bot, trip_num) -> int:
+    logger.info('center_button_event')
+    run_trip(bot, trip_num)
+    return trip_num
+
+trip_num_to_func = {
+    1: trip_01,
+    2: trip_02,
+    3: trip_03,
+    4: trip_04,
+    5: trip_05,
+    6: trip_06,
+    7: trip_07,
+    8: trip_08,
+}
+
+def run_trip(bot, trip_num):
+    if trip_num in trip_num_to_func:
+        return trip_num_to_func[trip_num](bot)
+    else:
+        return f"No function associated with trip_num {trip_num}"
 
 def main():
     bot = Robot()
+    bot.hub.system.set_stop_button(Button.BLUETOOTH)
+    trip_num = TRIP_START
+    prev_buttons = None
     while True:
-        # Set light to green if pressed, else red.
+        bot.hub.display.number(trip_num)
         buttons = bot.hub.buttons.pressed()
-        if Button.LEFT in buttons:
-            left_button_event(bot)
-        elif Button.RIGHT in buttons:
-            right_button_event(bot)
-        elif Button.BLUETOOTH in buttons:
-            bluetooth_button_event(bot)
-        elif Button.CENTER in buttons:
-            center_button_event(bot)
+        if buttons and not prev_buttons: 
+            # Only process the 1st event of a button press,
+            # As a single button press may trigger in multiple button events
+            if Button.LEFT in buttons:
+                trip_num = left_button_event(bot, trip_num)
+            elif Button.RIGHT in buttons:
+                trip_num = right_button_event(bot, trip_num)
+            elif Button.BLUETOOTH in buttons:
+                trip_num = bluetooth_button_event(bot, trip_num)
+            elif Button.CENTER in buttons:
+                trip_num = center_button_event(bot, trip_num)
+        prev_buttons = buttons
     print('event loop ended.')
 
 if __name__ == "__main__":
     main()
+############### DO NOT CHANGE ABOVE ###############
