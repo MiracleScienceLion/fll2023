e4d7319297d518371229ce3f2a88107d53705ce9
Shoupu Wan <swan@roblox.com>
2023-10-09 17:05:41 -0700

    create the trips

---
+++ pybrickscode/trip_02.py
@@ -0,0 +1,16 @@
+from pybricks.parameters import Button, Color, Direction, Icon, Port, Side, Stop
+
+from fll_robot import Robot
+from lib_turn import gyro_turn
+from lib_move import move
+from lib_line_follow import line_follow
+from lib_polygon import polygon
+
+
+def run(bot: Robot):
+    print("trip_02")
+
+
+if __name__ == "__main__":
+    bot = Robot()
+    run(bot)
