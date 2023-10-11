19f7c3b590fdea9c297870f9a70a6764a5bf0896
Samuel Wan <wanule123@gmail.com>
2023-10-10 20:53:01 -0700

    i revised the launch pad and thee location as well as fixing the giver hands and software

--- pybrickscode/trip_04.py
+++ pybrickscode/trip_04.py
@@ -1,18 +1,27 @@
-from lib_polygon import polygon
+from pybricks.parameters import Button, Color, Direction, Icon, Port, Side, Stop
+
 from fll_robot import Robot
+from lib_turn import gyro_turn
+from lib_move import move
+from lib_line_follow import line_follow
+from lib_polygon import polygon
 
 
 def run(bot: Robot):
     bot.reset_heading(-90)
     a_number = 340
-    polygon(bot, vertices=[(a_number, 1110), (a_number, 101)])
+    polygon(bot, vertices=[(a_number, 1050), (a_number, 190)])
     bot.left_motor.run_angle(800, -1300, wait=False)
     bot.right_motor.run_angle(800, -1290)
     bot.left_motor.run_angle(800, 1300, wait=False)
     bot.right_motor.run_angle(800, 1290)
-    polygon(bot, vertices=[(a_number, 105), (a_number, -1008)])
+    polygon(bot, vertices=[(a_number, 105), (a_number, -750)])
+
+
+def main():
+    bot = Robot()
+    run(bot)
 
 
 if __name__ == "__main__":
-    bot = Robot()
-    run(bot)
+    main()
