720286a8b0800e4524a70fe31acf9a65a629817a
David Tian <david.tian@gmail.com>
2023-10-14 09:45:01 -0700

    Add trip 7 missions.

--- pybrickscode/mission_09_movie.py
+++ pybrickscode/mission_09_movie.py
@@ -7,6 +7,16 @@ from lib_line_follow import line_follow
 
 def run(bot: Robot):
     print("mission_09_movie")
+    move(bot, distance_mm=250)
+    bot.right_motor.run_angle(speed=200, rotation_angle=220)
+    move(bot, distance_mm=-20, speed=200)
+    gyro_turn(bot, -50, speed=50)
+    move(bot, distance_mm=-50, speed=200)
+    bot.right_motor.run_angle(speed=900, rotation_angle=-190)
+    gyro_turn(bot, -20, speed=300) 
+    move(bot, distance_mm=330, speed=200) 
+    gyro_turn(bot, 90, speed=300) 
+    move(bot, distance_mm=3000, speed=200)
 
 def main():
     bot = Robot()
--- pybrickscode/trip_07.py
+++ pybrickscode/trip_07.py
@@ -5,11 +5,12 @@ from lib_turn import gyro_turn
 from lib_move import move
 from lib_line_follow import line_follow
 from lib_polygon import polygon
+import mission_09_movie
 
 
 def run(bot: Robot):
     # TODO implement this function
-    pass
+    mission_09_movie.run(bot)
 
 
 if __name__ == "__main__":
