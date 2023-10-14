d48da864ed910279d0a792866b5f4a9c784b4bcb
Chris Tian <chrislutian12@gmail.com>
2023-10-14 10:31:21 -0700

    changed to bot.straight() and bot.turn()

--- pybrickscode/mission_09_movie.py
+++ pybrickscode/mission_09_movie.py
@@ -1,22 +1,22 @@
 from pybricks.parameters import Button, Color, Direction, Icon, Port, Side, Stop
 
 from fll_robot import Robot
-from lib_turn import gyro_turn
+from lib_turn import turn
 from lib_move import move
 from lib_line_follow import line_follow
 
 def run(bot: Robot):
     print("mission_09_movie")
-    move(bot, distance_mm=250)
+    bot.straight(distance=250, speed=200)
     bot.right_motor.run_angle(speed=200, rotation_angle=220)
-    move(bot, distance_mm=-20, speed=200)
-    gyro_turn(bot, -50, speed=50)
-    move(bot, distance_mm=-50, speed=200)
+    bot.straight(distance=-30, speed=200)
+    bot.turn(angle=-50, turn_rate=100)
+    bot.straight(distance=-50, speed=200)
     bot.right_motor.run_angle(speed=900, rotation_angle=-190)
-    gyro_turn(bot, -20, speed=300) 
-    move(bot, distance_mm=330, speed=200) 
-    gyro_turn(bot, 90, speed=300) 
-    move(bot, distance_mm=3000, speed=200)
+    bot.turn(angle=-20, speed=300) 
+    bot.straight(distance=330, speed=200) 
+    bot.turn(angle=90, speed=300) 
+    bot.straight(distance=3000, speed=200)
 
 def main():
     bot = Robot()
